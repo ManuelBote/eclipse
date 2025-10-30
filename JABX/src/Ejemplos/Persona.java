b92428e26862dba1984511b6456750f9e68c3e5c
ManuelBote <mbotez01@educarex.es>
2025-10-30 08:46:05 +0100

    ejemplo javx

--- JABX/src/Ejemplos/Persona.java
+++ JABX/src/Ejemplos/Persona.java
@@ -1,4 +1,4 @@
-package Ejercicios;
+package Ejemplos;
 
 import javax.xml.bind.annotation.XmlType;
 
--- JABX/src/Ejemplos/Personas.java
+++ JABX/src/Ejemplos/Personas.java
@@ -1,4 +1,4 @@
-package Ejercicios;
+package Ejemplos;
 
 import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlElement;
