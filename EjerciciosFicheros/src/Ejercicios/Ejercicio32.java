1dca81c515755a6d13304af5fe99f225e46db80a
ManuelBote <mbotez01@educarex.es>
2025-10-31 10:01:11 +0100

    31/10

--- EjerciciosFicheros/src/Ejercicios/Ejercicio32.java
+++ EjerciciosFicheros/src/Ejercicios/Ejercicio32.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
+//@author Manuel Bote Zabala
 public class Ejercicio32 {
 
 	public static void main(String[] args) {
