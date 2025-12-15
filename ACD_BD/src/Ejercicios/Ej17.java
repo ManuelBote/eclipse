94b631566e98b2506dd1cc2813dca7692cabc21c
dam23 <dam23@10.0.2.15>
2025-12-15 13:32:40 +0100

    ej17

--- ACD_BD/src/Ejercicios/Ej17.java
+++ ACD_BD/src/Ejercicios/Ej17.java
@@ -1,9 +1,22 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class Ej17 {
 
+	private static Scanner tc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("¿Desea agregar los nuevos datos? (s/n)");
+		String cond = tc.nextLine();
+		
+		if(cond.toLowerCase().equals("s")) {
+			
+			
+			
+		}
+		
 
 	}
 
