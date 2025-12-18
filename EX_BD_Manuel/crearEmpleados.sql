49feed7e422ef5053c6fb063092adf0c9ec1d004
dam23 <dam23@10.0.2.15>
2025-12-18 10:47:29 +0100

    ex

---
+++ EX_BD_Manuel/crearEmpleados.sql
@@ -0,0 +1,7 @@
+CREATE TABLE empleados(
+    dni VARCHAR(9) PRIMARY KEY,
+    nombre VARCHAR(20),
+    edad INT,
+    departamento VARCHAR(40),
+    sueldo INT
+    );
---
+++ EX_BD_Manuel/insertarEmpleados.sql
@@ -0,0 +1,6 @@
+INSERT INTO empleados (dni, nombre, edad, departamento, sueldo) VALUES
+	('12345678A','Juan Pérez', 39, 'Ventas', 3000),
+    ('45678912B','Carlos Sánchez', 34, 'Marketing', 4500),
+    ('98765432S','Maria Rodríguez', 42, 'RRHH', 3500),
+    ('11122233V','Pedro Diaz', 29, 'Ventas', 2900),
+    ('58969696Z','Ana Gómez', 32, 'RRHH', 2500);
