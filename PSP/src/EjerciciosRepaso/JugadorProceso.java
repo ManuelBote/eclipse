f6a87f2fbe77e7907fa5c84d1a37ff30835bfe29
ManuelBote <mbotez01@educarex.es>
2026-01-13 09:25:23 +0100

    Bingo Procesos

--- PSP/src/EjerciciosRepaso/JugadorProceso.java
+++ PSP/src/EjerciciosRepaso/JugadorProceso.java
@@ -5,26 +5,27 @@ import java.util.HashSet;
 
 public class JugadorProceso {
     public static void main(String[] args) {
+    	
         int id = Integer.parseInt(args[0]);
-        HashSet<Integer> carton = new HashSet<>();
+        HashSet<Integer> carton = new HashSet<Integer>();
+        
         while (carton.size() != 15) {
             carton.add(1 + (int) (Math.random() * 99));
         }
+        
         System.out.println("Jugador " + id + " - Cartón: " + carton);
-        System.out.flush();  // ¡AÑADIDO!
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             String linea;
-            while ((linea = br.readLine()) != null) {
+            while (true) {
+            	linea = br.readLine();
                 if (linea.equals("FIN")) break;
                 int num = Integer.parseInt(linea);
                 if (carton.remove(num)) {
                     System.out.println("Jugador " + id + " tiene " + num);
-                    System.out.flush();  // ¡CLAVE!
                 }
                 if (carton.isEmpty()) {
                     System.out.println("GANADOR:" + id);
-                    System.out.flush();  // ¡CLAVE!
                     break;
                 }
             }
