59d2c0186d949945e621d2f35627c035e99d04db
dam23 <dam23@10.0.2.15>
2026-01-21 10:21:57 +0100

    ej13

--- ACD_3/src/Ejercicios/Ej13.java
+++ ACD_3/src/Ejercicios/Ej13.java
@@ -1,10 +1,34 @@
 package Ejercicios;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Ej13 {
 	
 	static Scanner ts = new Scanner(System.in);
+
+	//Metodo para hacer la conexion a la BD
+	public static Connection conectar() {
+		Connection conexion = null;
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			conexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mascota", "postgres", "toor");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return conexion;
+	}
+	
 	
 	//Menu opcion Alta
 	public static void menuAlta() {
@@ -94,8 +118,149 @@ public class Ej13 {
 		}while(condW);
 		System.out.println("Saliendo..");
 		
-		
-	
 	}
+	
+	//========================	
+	//===== METODOS ALTA =====
+	//========================
+	
+	//Alta Veterinario
+	public static void altaVeterinario() {
+		Connection conexion = conectar();
+		
+		try {
+			System.out.print("DNI del veterinario: ");
+			String dni = ts.nextLine();
+			System.out.print("Nombre del veterinario: ");
+			String nombre = ts.nextLine();
+			System.out.print("Nº de colegioalo del veterinario: ");
+			int numCol = ts.nextInt(); ts.nextLine();
+			
+			String sql = "INSERT INTO public.veterinario(dni, nombre, num_colegiado) VALUES (?, ?, ?)";
+			
+			PreparedStatement stmt = conexion.prepareStatement(sql);
+			stmt.setString(1, dni);	
+			stmt.setString(2, nombre);
+			stmt.setInt(3, numCol);
+			
+			// Ejecutar la consulta
+			int resultado = stmt.executeUpdate();
+			if (resultado > 0) {
+			System.out.println("¡Datos insertados con éxito!");
+			} else {
+			System.out.println("No se insertaron datos.");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Alta Masctota
+	public static void altaMascota() {
+		Connection conexion = conectar();
+		
+		try {		
+			System.out.print("Nombre de la mascota: ");
+			String nombre = ts.nextLine();
+			System.out.print("Especie de la mascota: ");
+			String especie = ts.nextLine();
+			System.out.print("Raza de la mascota: ");
+			String raza = ts.nextLine();
+
+			System.out.print("Cantidad de vacunas de la mascota: ");
+			int cant = ts.nextInt(); ts.nextLine();
+			
+			//Formacion del array
+			StringBuilder arrayVacunas = new StringBuilder("ARRAY[");
+			
+			for (int i = 0; i < cant; i++) {
+				System.out.println("Vacuna " + (i+1));
+				
+				System.out.print("Nombre de la vacuna: ");
+				String vacuna = ts.nextLine();
+				System.out.print("Numero de colegiado del veterinario: ");
+				int veterinario = ts.nextInt(); ts.nextLine();
+				System.out.print("Fecha de vacunacion(YYYY-MM-DD): ");
+				LocalDate fecha = LocalDate.parse(ts.nextLine());
+				
+				if(i > 0) {
+					arrayVacunas.append(", ");
+				}
+				arrayVacunas.append("('" + vacuna + "', '" + veterinario + "', '" + fecha + "')");	
+			}
+			arrayVacunas.append("]::cartilla_vacunacion[]");
+			
+			
+			String sql = "INSERT INTO public.mascota (nombre, especie, raza, vacuna)"
+					+ "VALUES (?, ?, ?, "+ arrayVacunas.toString() +")";
+			
+			PreparedStatement stmt = conexion.prepareStatement(sql);
+			stmt.setString(1, nombre);	
+			stmt.setString(2, especie);
+			stmt.setString(3, raza);
+
+			
+			// Ejecutar la consulta
+			int resultado = stmt.executeUpdate();
+			if (resultado > 0) {
+			System.out.println("¡Datos insertados con éxito!");
+			} else {
+			System.out.println("No se insertaron datos.");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//AltaPropietario
+	public static void insertarPropietario() {
+		Connection conexion = conectar();
+		
+		try {
+			System.out.print("DNI del propietario: ");
+			String dni = ts.nextLine();
+			System.out.print("Nombre del propietario: ");
+			String nombre = ts.nextLine();
+			System.out.print("Calle del propietario: ");
+			String calle = ts.nextLine();
+			System.out.print("Nuemero del domicilio del propietario: ");
+			int num = ts.nextInt(); ts.nextLine();
+			System.out.print("Ciudad del propietario: ");
+			String ciudad = ts.nextLine();
+			System.out.print("Codigo postal: ");
+			int cp = ts.nextInt(); ts.nextLine();
+			
+			String sql = "INSERT INTO public.propietario(dni, nombre, direccion) VALUES (?, ?, (?, ?, ?, ?))";
+			
+			PreparedStatement stmt = conexion.prepareStatement(sql);
+			stmt.setString(1, dni);	
+			stmt.setString(2, nombre);
+			stmt.setString(3, calle);
+			stmt.setInt(4, num);
+			stmt.setString(5, ciudad);
+			stmt.setInt(6, cp);
+			
+			// Ejecutar la consulta
+			int resultado = stmt.executeUpdate();
+			if (resultado > 0) {
+			System.out.println("¡Datos insertados con éxito!");
+			} else {
+			System.out.println("No se insertaron datos.");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//=============================	
+	//===== METODOS MODIFICAR =====
+	//=============================
+	
+	//Modificar Propietario
+	
 
 }
